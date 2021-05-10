3c3eba3a36087186372f7b635b6dc44229bd25a2
orangepi_2106 <cot1884@gmail.com>
2021-05-10 08:41:54 +0500

    door closing

--- sh/init_gpio.sh
+++ sh/init_gpio.sh
@@ -7,10 +7,16 @@ do
 done
 
 for x in $PINS
-    do
-	echo -n 0 > /sys/class/gpio/gpio$x/value
-	echo "$v > $x"
-	sleep 1
-    done
+do
+    echo -n 0 > /sys/class/gpio/gpio$x/value
+    echo "$v > $x"
+    sleep 0.1
+done
 
 echo -n 1 > /sys/class/gpio/gpio0/value
+
+for x in $PINS
+do
+    chgrp -R 1000 /sys/devices/platform/sunxi-pinctrl/gpio/gpio$x
+    chmod g+w /sys/devices/platform/sunxi-pinctrl/gpio/gpio$x/value
+done
--- sh/open_door.sh
+++ sh/open_door.sh
@@ -1,13 +1,10 @@
 
-for i in 1, 2
+seq 5 | while read x
 do
-for i in 1, 2, 3, 4
-do
-echo -n 1 > /sys/class/gpio/gpio1/value
-echo -n 0 > /sys/class/gpio/gpio107/value
-sleep 0.00001
-echo -n 0 > /sys/class/gpio/gpio1/value
-echo -n 0 > /sys/class/gpio/gpio107/value
-done
-
+    echo -n 1 > /sys/class/gpio/gpio1/value
+    echo -n 0 > /sys/class/gpio/gpio107/value
+    sleep 0.02
+    echo -n 0 > /sys/class/gpio/gpio1/value
+    echo -n 0 > /sys/class/gpio/gpio107/value
+    sleep 0.2
 done
